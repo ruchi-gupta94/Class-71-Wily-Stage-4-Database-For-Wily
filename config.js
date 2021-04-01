d48fe45610f05edf334163f36319bf75f8e72e83
Ruchi Gupta <70604743+ruchi-gupta94@users.noreply.github.com>
2021-04-01 08:05:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
-import * as firebase from 'firebase'
-require('@firebase/firestore')
+import firebase from 'firebase';
+require('@firebase/firestore');
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBI_6KhkBRmacZ1wuIDJYA-H53CXyau-P0",
-    authDomain: "willy-app-f2f57.firebaseapp.com",
-    databaseURL: "https://willy-app-f2f57.firebaseio.com",
-    projectId: "willy-app-f2f57",
-    storageBucket: "willy-app-f2f57.appspot.com",
-    messagingSenderId: "957572252876",
-    appId: "1:957572252876:web:c77c38bb82a60591efee69",
-    measurementId: "G-4Y08PYMXK1"
-  };
-  // Initialize Firebase
+  apiKey: "AIzaSyDLi4RyfXpC4StVAtN3bK_h9YJKxQ_KxYk",
+  authDomain: "wily-app-2816a.firebaseapp.com",
+  projectId: "wily-app-2816a",
+  storageBucket: "wily-app-2816a.appspot.com",
+  messagingSenderId: "765006760610",
+  appId: "1:765006760610:web:886ab4d4fe911bda5c65fb"
+};
+  
+//Initialize Firebase
+if(!firebase.apps.length){
   firebase.initializeApp(firebaseConfig);
+}
 
-  export default firebase.firestore();
+export default firebase.firestore();
